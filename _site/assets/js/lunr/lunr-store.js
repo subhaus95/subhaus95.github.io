910f84903f105fa3a1125c1ade070a5a00cbab20
Paul Hobson <p.d.hobson@icloud.com>
2025-02-25 15:46:09 -0700

    v0.1 commit
    Should be good to go for the basics

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,25 @@
-var store = []
+var store = [{
+        "title": "Back To The Future",
+        "excerpt":"I’ve decided to resurrect the Breadedcod.com site after a fairly lengthy hiatus. I can’t even remember when the old Wordpress blog went by the wayside, but it’s quite some time ago. *I just checked, it’s been a few years according to the ol’ reliable of archive.org. Anyway, ‘HI!’ again, for...","categories": [],
+        "tags": [],
+        "url": "/Back-to-the-Future/",
+        "teaser": null
+      },{
+        "title": "Winter Camping",
+        "excerpt":"Last weekend saw us out at Water Valley towards the foothills for our annual Winter Camp. Of course, being as Albertan weather tends to be winter when it feels like it, it was actually colder back in our November ‘Fall’ camp than we experienced just. No problem though. -16C overnight...","categories": [],
+        "tags": [],
+        "url": "/Winter-Camping/",
+        "teaser": null
+      },{
+        "title": "Ski Camp",
+        "excerpt":"I’m writing this post through a different lens than I would have done twenty-four hours ago. With everything going on in the world at present, there is some guilt mixed in with the sheer joy of spending time with a bunch of youth at their Ski Camp this past weekend....","categories": [],
+        "tags": [],
+        "url": "/Ski-Camp/",
+        "teaser": null
+      },{
+        "title": "Rhys",
+        "excerpt":"I have written and deleted the start of this post more times than I care to admit this morning. It’s hard to encapsulate how I feel when the news that Rhys passed away is still so fresh. I feel numb. I posted to Twitter this morning with a photo of...","categories": [],
+        "tags": [],
+        "url": "/rhys/",
+        "teaser": null
+      }]
