7b76c52c81cf3555612ec1d9df512099c3778081
subhaus95 <subhaus95@users.noreply.github.com>
2025-03-04 12:32:34 +0000

    deploy: subhaus95/subhaus95-jekyll-source@4299607c961e1733787a53438fc02fb01399ea5f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,30 +2,30 @@ var store = [{
         "title": "Back To The Future",
         "excerpt":"I’ve decided to resurrect the Breadedcod.com site after a fairly lengthy hiatus. I can’t even remember when the old Wordpress blog went by the wayside, but it’s quite some time ago. *I just checked, it’s been a few years according to the ol’ reliable of archive.org. Anyway, ‘HI!’ again, for...","categories": [],
         "tags": [],
-        "url": "/Back-to-the-Future/",
+        "url": "/2022/02/01/Back-to-the-Future/",
         "teaser": null
       },{
         "title": "Winter Camping",
         "excerpt":"Last weekend saw us out at Water Valley towards the foothills for our annual Winter Camp. Of course, being as Albertan weather tends to be winter when it feels like it, it was actually colder back in our November ‘Fall’ camp than we experienced just. No problem though. -16C overnight...","categories": [],
         "tags": [],
-        "url": "/Winter-Camping/",
+        "url": "/2022/02/02/Winter-Camping/",
         "teaser": null
       },{
         "title": "Ski Camp",
         "excerpt":"I’m writing this post through a different lens than I would have done twenty-four hours ago. With everything going on in the world at present, there is some guilt mixed in with the sheer joy of spending time with a bunch of youth at their Ski Camp this past weekend....","categories": [],
         "tags": [],
-        "url": "/Ski-Camp/",
+        "url": "/2022/03/06/Ski-Camp/",
         "teaser": null
       },{
         "title": "Rhys",
         "excerpt":"I have written and deleted the start of this post more times than I care to admit this morning. It’s hard to encapsulate how I feel when the news that Rhys passed away is still so fresh. I feel numb. I posted to Twitter this morning with a photo of...","categories": [],
         "tags": [],
-        "url": "/rhys/",
+        "url": "/2022/03/07/rhys/",
         "teaser": null
       },{
-        "title": "Why I Chose Obsidian. From Pandemic Discovery To Daily Driver",
+        "title": "Why I Chose Obsidian.",
         "excerpt":"Why I Chose Obsidian: From Pandemic Discovery to Daily Driver When the pandemic hit in early 2020, like many of us, I suddenly found myself with an abundance of unexpected free time. With no real expectation that I would be returning to the role I was stood down from, I...","categories": [],
         "tags": [],
-        "url": "/Why-I-Chose-Obsidian.-From-Pandemic-Discovery-to-Daily-Driver/",
+        "url": "/2025/03/03/Why-I-Chose-Obsidian",
         "teaser": null
       }]
